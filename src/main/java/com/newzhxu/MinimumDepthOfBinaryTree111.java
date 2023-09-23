a6e61b1be8ec48e134bb0ff867eb743463a24ce1
zahl <zheng2580369@gmail.com>
2023-09-24 01:48:54 +0800

    111. 二叉树的最小深度

--- src/main/java/com/newzhxu/MinimumDepthOfBinaryTree111.java
+++ src/main/java/com/newzhxu/MinimumDepthOfBinaryTree111.java
@@ -49,15 +49,18 @@ public class MinimumDepthOfBinaryTree111 {
     }
 
     int minDepth(TreeNode root) {
-        if (root == null) return 0;
-//        int m1 = minDepth(root.left);
-//        int m2 = minDepth(root.right);
-        //1.如果左孩子和右孩子有为空的情况，直接返回m1+m2+1
-        //2.如果都不为空，返回较小深度+1
-        return root.left == null || root.right == null ?
-                minDepth(root.left) + minDepth(root.right) + 1 : Math.min(minDepth(root.left), minDepth(root.right)) + 1;
-
-
+        if (root == null) {
+            return 0;
+        }
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        if (root.left == null && root.right != null) {
+            return right + 1;
+        } else if (root.left != null && root.right == null) {
+            return left + 1;
+        } else {
+            return Math.min(left, right) + 1;
+        }
     }
 
     @AllArgsConstructor
